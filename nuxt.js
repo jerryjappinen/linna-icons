f6c4b179ba97313e0d5efe6fcf1bdc509c316952
Jerry Jäppinen <eiskis@gmail.com>
2022-07-24 17:14:31 +0200

    Meta data for module

--- nuxt.js
+++ nuxt.js
@@ -1,19 +1,27 @@
 import { defineNuxtModule } from '@nuxt/kit'
 import { fileURLToPath } from 'node:url'
 
-// This file allows using linna-vue as a Nuxt module
+// This file allows auto importing linna-icons in a Nuxt app
 // Add this to nuxt.config:
 //   modules: [
-//     'linna-icons'
+//     'linna-icons/nuxt'
 //   ]
 export default defineNuxtModule({
+  meta: {
+    name: 'linna-icons',
+    configKey: 'linna-icons',
+    compatibility: {
+      nuxt: '^3.0.0'
+    }
+  },
+
   hooks: {
     'components:dirs' (dirs) {
       // Add ./components dir to the list
       dirs.push({
         path: fileURLToPath(new URL('./', import.meta.url)),
         extensions: ['svg', 'vue'],
-        prefix: 'Icon'
+        prefix: 'Icon' // NOTE: need to move this to setup function to support an adjustable prefix
       })
     }
   }
